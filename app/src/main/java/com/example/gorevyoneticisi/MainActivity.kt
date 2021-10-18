eac756e3f545889197f4a303e0cd64b8e40af0b8
ibrahimertanylmz <ibrahimertanylmz@gmail.com>
2021-10-18 18:00:32 +0300

    Database classes are updated. MainActivity created. TaskViewModel updated.

--- app/src/main/java/com/example/gorevyoneticisi/MainActivity.kt
+++ app/src/main/java/com/example/gorevyoneticisi/MainActivity.kt
@@ -2,10 +2,57 @@ package com.example.gorevyoneticisi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.gorevyoneticisi.adapter.TaskRecyclerViewAdapter
+import com.example.gorevyoneticisi.database.Task
+import com.example.gorevyoneticisi.database.TaskDatabase
+import com.example.gorevyoneticisi.database.TaskRepository
+import com.example.gorevyoneticisi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var taskViewModel: TaskViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        val dao = TaskDatabase.getInstance(application).taskDAO
+        val repository = TaskRepository(dao)
+        val factory = TaskViewModelFactory(repository)
+        taskViewModel = ViewModelProvider(this,factory).get(TaskViewModel::class.java)
+
+        binding.myViewModel = taskViewModel
+        binding.lifecycleOwner = this
+
+        initRecyclerView()
+
+        //This is how you display Messages in MVVM architecture
+        taskViewModel.message.observe(this, Observer {
+            it.getContentIfNotHandled()?.let {
+                Toast.makeText(this,it, Toast.LENGTH_LONG).show()
+            }
+        })
+    }
+
+    // observe that live data from the repository
+    private fun displayTaskList(){
+        taskViewModel.tasks.observe(this, Observer {
+            Log.i("myTag",it.toString())
+            binding.taskRecyclerView.adapter = TaskRecyclerViewAdapter(it,{selectedItem:Task->listItemClicked(selectedItem)})
+        })
+    }
+    private fun initRecyclerView(){
+        binding.taskRecyclerView.layoutManager = LinearLayoutManager(this)
+        displayTaskList()
+    }
+    private fun listItemClicked(task: Task){
+        Toast.makeText(this,"selected name is ${task.description}",Toast.LENGTH_LONG).show()
+        taskViewModel.initUpdateAndDelete(task)
     }
 }
--- app/src/main/java/com/example/gorevyoneticisi/TaskViewModel.kt
+++ app/src/main/java/com/example/gorevyoneticisi/TaskViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.gorevyoneticisi
 
+import androidx.databinding.Bindable
 import androidx.databinding.Observable
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
@@ -11,21 +12,96 @@ import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 
 class TaskViewModel (private val repository: TaskRepository): ViewModel(), Observable {
-    val subscribers = repository.subscribers
+    val tasks = repository.tasks
+    private var isUpdateOrDelete = false
+    private lateinit var taskToUpdateOrDelete : Task
+
+    @Bindable // for this check activity_main.xml and ViewModel.inputName
+    val inputName = MutableLiveData<String>()
+    @Bindable
+    val inputEmail = MutableLiveData<String>()
+    @Bindable
+    val saveOrUpdateButtonText = MutableLiveData<String>()
+    @Bindable
+    val  clearAllOrDeleteButtonText = MutableLiveData<String>()
 
     private val statusMessage = MutableLiveData<Event<String>>()
 
     val message : LiveData<Event<String>>
         get() = statusMessage
 
+    init {
+        saveOrUpdateButtonText.value="Save"
+        clearAllOrDeleteButtonText.value="Clear All"
+    }
+
+    fun saveOrUpdate(){
+        if(isUpdateOrDelete){
+            taskToUpdateOrDelete.description = inputName.value!!
+            taskToUpdateOrDelete.period = inputEmail.value!!
+            update(taskToUpdateOrDelete)
+        }
+        else{
+            val name =inputName.value!!
+            val email =inputEmail.value!!
+            insert(Task(0,name,email))
+            inputName.value = null
+            inputEmail.value = null
+        }
+    }
+    fun clearOrDelete(){
+        if(isUpdateOrDelete){
+            delete(taskToUpdateOrDelete)
+        }
+        else{
+            clearAll()
+        }
+    }
     fun insert(task: Task): Job = viewModelScope.launch {
         val newRowId =repository.insert(task)
         if (newRowId>-1){
-            statusMessage.value = Event("Subscriber Inserted Successfully!! $newRowId ")
+            statusMessage.value = Event("Task Inserted Successfully!! $newRowId ")
         }else{
             statusMessage.value = Event("Error Occurred!!")
         }
 
+    }
+    fun update(task: Task): Job = viewModelScope.launch {
+        val noOfRows = repository.update(task)
+        if(noOfRows>0) {
+            inputName.value = null
+            inputEmail.value = null
+            isUpdateOrDelete = false
+            saveOrUpdateButtonText.value = "Save"
+            clearAllOrDeleteButtonText.value = "Clear All"
+            statusMessage.value = Event("Task Updated Successfully!!")
+        }
+        else{
+            statusMessage.value = Event("Error Occurred!!")
+        }
+
+    }
+    fun delete(task: Task): Job = viewModelScope.launch {
+        repository.delete(task)
+        inputName.value = null
+        inputEmail.value = null
+        isUpdateOrDelete = false
+        saveOrUpdateButtonText.value = "Save"
+        clearAllOrDeleteButtonText.value = "Clear All"
+        statusMessage.value = Event("Task Deleted Successfully!!")
+
+    }
+    fun initUpdateAndDelete(task: Task){
+        inputName.value = task.description
+        inputEmail.value = task.period
+        isUpdateOrDelete = true
+        taskToUpdateOrDelete = task
+        saveOrUpdateButtonText.value = "Update"
+        clearAllOrDeleteButtonText.value = "Delete"
+    }
+    fun clearAll() : Job =viewModelScope.launch {
+        repository.deleteAll()
+        statusMessage.value = Event("All Tasks Deleted Successfully!!")
     }
 
     override fun addOnPropertyChangedCallback(callback: Observable.OnPropertyChangedCallback?) {
