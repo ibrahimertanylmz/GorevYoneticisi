1d3cc5b37c1ad1fd24da36262c5799cc50c2e07d
İbrahim Ertan Yılmaz <ibrahimertanylmz@gmail.com>
2021-10-18 12:19:17 +0300

    ViewModel and ViewModelFactory classes are added.

---
+++ app/src/main/java/com/example/gorevyoneticisi/TaskViewModelFactory.kt
@@ -0,0 +1,14 @@
+package com.example.gorevyoneticisi
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.gorevyoneticisi.database.TaskRepository
+
+class TaskViewModelFactory (private val repository: TaskRepository): ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom((TaskViewModel::class.java))){
+            return TaskViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Unknown View Model Class")
+    }
+}
